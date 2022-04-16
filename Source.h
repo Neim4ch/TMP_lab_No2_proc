a22d1dbe51cf1f28838f7bcc4ffc040ae0a733f4
Neim4ch <64603880+Neim4ch@users.noreply.github.com>
2022-04-16 23:33:09 +0700

    added new class

--- Source.h
+++ Source.h
@@ -1,44 +1,48 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+
 using namespace std;
-class film {
-public:
-	static film* In(ifstream& ifst);
-	virtual void InData(ifstream& ifst) = 0;
-	virtual void Out(ofstream& ofst) = 0;
+
+enum type { animation, feature, documentary };
+struct film {
+	void* obj = NULL;
+	type key;
+
 };
-class Node {
-public:
-	film* pic;
-	Node* next;
-	Node(film* newfigure);
+
+struct Node {
+	film* fl = NULL;
+	Node* next = NULL;
 };
-class feature : public film {
-	string director;
-public:
-	void InData(ifstream& ifst);
-	void Out(ofstream& ofst);
-	feature() {}
+
+struct feature_film {
+	string director = "";
 };
-class animation : public film {
-	enum way { DRAWN, DOLL, STOP_MOTION };
+
+struct documentary_film {
+	int year = 0;
+};
+
+enum way { DRAWN, DOLL, STOP_MOTION };
+struct animation_film {
 	way woc;
-public:
-	void InData(ifstream& ifst);
-	void Out(ofstream& ofst);
-	animation() {}
 };
-class container {
 
-public:
-	Node* head;
-	Node* curr;
-	int size;
-
-	void In(ifstream& ifst);
-	void Out(ofstream& ofst);
-	void Clear();
-	container();
-	~container() { Clear(); }
+struct container {
+	int size = 0;
+	Node* head = NULL;
+	Node* curr = NULL;
 };
+
+void In(ifstream& ifst, feature_film& f);
+void Out(ofstream& ofst, feature_film& f);
+void In(ifstream& ifst, animation_film& a);
+void Out(ofstream& ofst, animation_film& a);
+void In(ifstream& ifst, documentary_film& d);
+void Out(ofstream& ofst, documentary_film& d);
+film* InFilm(ifstream& ifst);
+film* OutFilm(ifstream& ifst);
+void Clear(container* c);
+void InCont(ifstream& ifst, container* c);
+void OutCont(ofstream& ofst, container* c);
